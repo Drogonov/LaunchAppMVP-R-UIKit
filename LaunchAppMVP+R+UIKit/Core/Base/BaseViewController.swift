8bb66d8d888e9f175fab92456911f4c4fb1d3d5c
Anton Drogonov <vlezko.anton@yandex.ru>
2021-12-10 17:26:55 +0300

    set app to dev

---
+++ LaunchAppMVP+R+UIKit/Core/Base/BaseViewController.swift
@@ -0,0 +1,99 @@
+//
+//  BaseViewController.swift
+//  LaunchAppMVP+R+UIKit
+//
+//  Created by Anton Vlezko on 10.12.2021.
+//
+
+import UIKit
+import SwiftUI
+
+protocol BaseViewLoader: AnyObject {
+    func showLoader()
+    func hideLoader()
+}
+
+
+class BaseViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private lazy var spinner = BaseLoaderViewController()
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.backgroundColor
+        configureNavigationBar()
+        configureSwipeGesture()
+        self.hideKeyboardWhenTappedAround()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleSwipeLeftGesture(gesture: UISwipeGestureRecognizer) -> Void {}
+
+    @objc func handleSwipeRightGesture(gesture: UISwipeGestureRecognizer) -> Void {}
+
+    // MARK: - Open methods
+        
+    func setNavigationBarTitle(with title: String) {
+        self.navigationItem.title = title
+    }
+    
+    func addMainViewToViewController<T: View>(_ newView: T) {
+        let viewCtrl = UIHostingController(rootView: newView)
+        addChild(viewCtrl)
+        view.addSubview(viewCtrl.view)
+        
+        viewCtrl.view.anchor(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            leading: view.safeAreaLayoutGuide.leftAnchor,
+            bottom: view.safeAreaLayoutGuide.bottomAnchor,
+            trailing: view.safeAreaLayoutGuide.rightAnchor
+        )
+        viewCtrl.didMove(toParent: self)
+    }
+    
+    // MARK: - Private methods
+    
+    private func configureNavigationBar() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.tintColor = UIColor.primaryTextColor
+        self.navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.accentColor]
+    }
+    
+    private func configureSwipeGesture() {
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeLeftGesture))
+        swipeLeft.direction = .left
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeRightGesture))
+        swipeLeft.direction = .right
+
+        self.view.addGestureRecognizer(swipeLeft)
+        self.view.addGestureRecognizer(swipeRight)
+
+    }
+}
+
+extension BaseViewController: BaseViewLoader {
+    func showLoader() {
+        addChild(spinner)
+        spinner.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(spinner.view)
+        spinner.spinner.startAnimation()
+        spinner.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        spinner.view.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        spinner.view.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        spinner.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        spinner.didMove(toParent: self)
+    }
+    
+    func hideLoader() {
+        spinner.spinner.stopAnimation()
+        spinner.willMove(toParent: nil)
+        spinner.view.removeFromSuperview()
+        spinner.removeFromParent()
+    }
+}
---
+++ LaunchAppMVP+R+UIKit/Utils/Extension + UIColor.swift
@@ -0,0 +1,23 @@
+//
+//  Extension + UIColor.swift
+//  LaunchAppMVP+R+UIKit
+//
+//  Created by Anton Vlezko on 10.12.2021.
+//
+
+import UIKit
+
+extension UIColor {
+    static let backgroundColor = UIColor.systemBackground
+    
+    static let primaryTextColor = UIColor.label
+    static let secondaryTextColor = UIColor.gray
+    
+    static let actionButtonTextColor = UIColor.systemBackground
+    static let actionButtonBackgroundColor = UIColor.label
+    
+    static let hologramColor1 = UIColor(patternImage: UIImage(named: "hologram_1")!)
+    static let hologramColor2 = UIColor(patternImage: UIImage(named: "hologram_2")!)
+    
+    static let accentColor = UIColor.red
+}
