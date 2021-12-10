6e3acf973b2799603917d5013e9fe452145ca4db
Anton Drogonov <vlezko.anton@yandex.ru>
2021-12-10 15:51:18 +0300

    Initial Commit

---
+++ LaunchAppMVP+R+UIKitTests/LaunchAppMVP_R_UIKitTests.swift
@@ -0,0 +1,33 @@
+//
+//  LaunchAppMVP_R_UIKitTests.swift
+//  LaunchAppMVP+R+UIKitTests
+//
+//  Created by Anton Vlezko on 10.12.2021.
+//
+
+import XCTest
+@testable import LaunchAppMVP_R_UIKit
+
+class LaunchAppMVP_R_UIKitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
